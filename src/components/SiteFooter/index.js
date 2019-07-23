12eba6954037de160fbc26d8a58097208b895e0c
Justin Cox <justincox@Violmusio.attlocal.net>
2019-07-23 12:44:27 -0500

    Modify footer to reflect disclaimer about usage

--- src/components/SiteFooter/index.js
+++ src/components/SiteFooter/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import './SiteFooter.css'
 
 const SiteFooter = props => {
   return (
     <div>
       <Card className="mt-4">
         <Card.Body>
-          <span>&copy; Copyright 2019 - Design &amp; Development by Justin Cox</span>
+          <span className="SITEFOOTER_disclaimer">This application is for academic purposes only. All credit goes to <a href="https://rawg.io">https://rawg.io</a> for use of the RAWG API</span>
         </Card.Body>
       </Card>
     </div>
